acbbbc003c5f54b1658940074c069e778a9896d7
PackeTsar <jkerns@packetsar.com>
2020-05-27 14:55:14 -0700

    Added clean of venv

--- clean.sh
+++ clean.sh
@@ -4,4 +4,6 @@
 
 find . -type d -name __pycache__ -prune -execdir rm -rf {} \;  # Removal all '__pycache__' directories and their contents
 
+find . -type d -name env -prune -execdir rm -rf {} \;  # Removal all '__pycache__' directories and their contents
+
 find . -type f -name "*.pyc" -delete  # Removal all '.pyc' directories and their contents
