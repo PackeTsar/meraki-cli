973b9e31ca3c615940da8bbeed3e915f1fa25d3d
John W Kerns <jkerns@packetsar.com>
2021-02-03 20:02:17 -0800

    Versioned to v1.0.1

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Quick Install: `pip install meraki-cli`
 
 -----------------------------------------
 ## VERSION
-The version of Meraki-CLI documented here is: **0.3.0**
+The version of Meraki-CLI documented here is: **1.0.1**
 
 
 -----------------------------------------
--- commit.sh
+++ commit.sh
@@ -4,14 +4,10 @@ bash clean.sh
 nano meraki_cli/__version__.py
 python3 -m venv commit_env
 source ./commit_env/bin/activate
-python3 -m pip install --upgrade pip
-pip3 install wheel
-pip3 install twine
+python3 -m pip install wheel
+python3 -m pip install twine
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
-
-read -p "Press ENTER to continue"
-
 
 deactivate
 
