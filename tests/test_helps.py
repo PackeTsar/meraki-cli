6e03a7324885c7adf339de0fdf5e4fb30d2cddc1
John W Kerns <jkerns@packetsar.com>
2021-02-08 07:43:51 -0800

    Removed. may add back later

--- tests/test_helps.py
+++ tests/test_helps.py
@@ -9,9 +9,3 @@ class TestHelps(unittest.TestCase):
         assert _cmd_title('oneTwoThree') == 'One Two Three'
         assert _cmd_title('OneTwoThree') == 'One Two Three'
         assert _cmd_title('onetwoThree') == 'Onetwo Three'
-
-    # def testHelp(self):
-    #    value = '\n    Some function documentation here\n    \n'\
-    #            '        >>> def Function(pos1: str, pos2: int, keyw1=False,'\
-    #            ' keyw2={}, *args, **kwargs):'
-    #    assert _cmd_help(Args(Function)) == value
