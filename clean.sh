45320e866c64ca0ac465744bb49dc2f44bc6216d
John W Kerns <jkerns@packetsar.com>
2021-02-08 11:36:20 -0800

    Fixed for duplicate caches

--- clean.sh
+++ clean.sh
@@ -20,7 +20,7 @@ rm -rf "coverage.xml"
 
 rm -rf "htmlcov"
 
-find . -type d -name __pycache__ -prune -execdir rm -rf {} \;  # Removal all '__pycache__' directories and their contents
+find . -type d -name "__pycache__*" -prune -execdir rm -rf {} \;  # Removal all '__pycache__' directories and their contents
 
 find . -type f -name "*.pyc" -delete  # Removal all '.pyc' files
 
