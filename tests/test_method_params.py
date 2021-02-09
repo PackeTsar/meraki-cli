03cfc8d7e2fc4a4f391b4b6fea96aed5da2b9ee2
John W Kerns <jkerns@packetsar.com>
2021-02-08 17:40:54 -0800

    Exercise error paths

--- tests/test_method_params.py
+++ tests/test_method_params.py
@@ -10,15 +10,23 @@ class TestMethodParams(unittest.TestCase):
     def setUp(self):
         self.parsed_args = ParsedArgs()
         self.arg_obj = Args(Function)
+        self.arg_dict = {'pos1': '100', 'pos2': 200}
 
     # Adjust the NO_STDIN global to be true so program ignores all STDIN
     @patch('meraki_cli.__main__.NO_STDIN', True)
     def testMethodParamsSuccess(self):
-        arg_dict = {'pos1': '100', 'pos2': 200}
-        output = _get_method_params(self.parsed_args, arg_dict, self.arg_obj)
+        output = _get_method_params(self.parsed_args, self.arg_dict,
+                                    self.arg_obj)
         assert output == (['100', 200], {'address': '192.168.1.1'})
 
-    def testMethodParamsError(self):
-        arg_dict = {'pos1': '100'}
+    @patch('meraki_cli.__main__.NO_STDIN', True)
+    def testMethodParamsBadKwargsError(self):
+        self.parsed_args.kwargs = 'Some Bad JSON here'
+        with self.assertRaises(SystemExit):
+            _get_method_params(self.parsed_args, self.arg_dict,
+                               self.arg_obj)
+
+    def testMethodParamsMissingArgError(self):
+        arg_dict = {'pos1': '100'}  # Should error due to missing required arg
         with self.assertRaises(SystemExit):
             _get_method_params(self.parsed_args, arg_dict, self.arg_obj)
